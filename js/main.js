e4be972c16505511e81f83cf9d1ba08281c689d8
Piotr Lewinski <piotrlew5@wp.pl>
2017-09-15 09:01:10 +0200

    automatyczny slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 $(function () {
-    
+
     console.log('hej');
     /*zmienne*/
     var slideCount = $('.single-slide').length;
@@ -21,48 +21,60 @@ $(function () {
     });
 
     function slide(newSlideIndex) {
-        
-       if(newSlideIndex < 0 || newSlideIndex >= slideCount) {
+
+        if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
             return;
         }
-        
+
         var marginLeft = (newSlideIndex * (-100)) + '%';
-        
-        slideShow.animate({'margin-left': marginLeft}, 800, function() {
-                          slideIndex = newSlideIndex;
-                          }); 
+
+        slideShow.animate({
+            'margin-left': marginLeft
+        }, 800, function () {
+            slideIndex = newSlideIndex;
+        });
     }
-    
-    
-//    szerokośc kontenera wynosząca ilość slajdów
-    
+
+
+    //    szerokośc kontenera wynosząca ilość slajdów
+
     slideShow.css('width', slideCount * 100 + '%');
-    
-    
-//    szerokosc i położenie każdego slajdu (marginesy)
-    
-    $('.single-slide').each(function(index) {
-        $(this).css({'width': slideWidth + '%', 'margin-left': index * slideWidth + '%'});
+
+
+    //    szerokosc i położenie każdego slajdu (marginesy)
+
+    $('.single-slide').each(function (index) {
+        $(this).css({
+            'width': slideWidth + '%',
+            'margin-left': index * slideWidth + '%'
+        });
     });
-    
-    
-//    uruchomienie funkcji po kliknięciu na strzałkę
-    
-   /* $('.prev-slide').click(function(){
-        slide(slideIndex - 1);
-    });
-    
-    */
-    
-    $(function carousel(){
-        slide(slideIndex + 1);
-        setTimeout(carousel, 1000);
-        
-    });
-    
-  
-    
-    
+
+
+    //    uruchomienie funkcji po kliknięciu na strzałkę
+
+    /* $('.prev-slide').click(function(){
+         slide(slideIndex - 1);
+     });
+     
+     */
+
 
     
+    slideIndex = 0;
+    carousel();
+
+    function carousel() {
+       
+     
+        slideIndex++;
+        slide(slideIndex);
+        if (slideIndex > slideCount) {
+            slideIndex = 1;
+        }
+        
+        setTimeout(carousel, 3000);
+    };
+
+
 });
