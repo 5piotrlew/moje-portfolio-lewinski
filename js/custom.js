9ae6150a6f69767aa110690ebb4c2f7737e70115
Piotr Lewinski <piotrlew5@wp.pl>
2017-09-22 13:50:40 +0200

    poprawiony skrypt fade-out

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,12 @@
+'use strict';
 /* efekt opacity z opoznieniem zmienic na scroll cos tam */
 
 document.body.className += " fade-out";
-function usun() {    
-    document.querySelector('body').classList.remove('fade-out');    
-}
 
-function hej() {
-    'use strict';
-    setTimeout(usun, 1);
-}
-window.onload = hej();
+window.onload = setTimeout(function () {
+    document.querySelector('body').classList.remove('fade-out');
+}, 1);
+;
 
 
 /* koniec efektu */
